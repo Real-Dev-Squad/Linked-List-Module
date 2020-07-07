ea3155477d8a91e6a775a59010f5035d15519684
invalidtoken <codeit.sahil98@gmail.com>
2020-07-07 23:00:33 +0530

    adding first test

--- tests/tests.js
+++ tests/tests.js
@@ -1,14 +1,37 @@
 const LL = require("../constructors/linked-list");
 const { beautifyLogs } = require("../helper-functions/beautifyLogs");
 
-// Adding Items in the start
 (function AddingItemsInTheStart() {
-  let newLL = new LL(0);
-  if (newLL.get(0).data === 0) {
-    console.log("Test running fine");
-  } else {
-    console.log("Test Not Running fine");
+  let testName = "Test for adding data is linked list";
+  let newLL = new LL();
+
+  // [[index, value]]
+  let arr = [
+    [0, 1],
+    [1, 9],
+    [2, 9],
+    [3, 20],
+  ];
+
+  // Adding data in LL
+  for (let el of arr) {
+    let index = el[0];
+    let data = el[1];
+    newLL.add(data, index);
   }
-  console.log(newLL);
+
+  // Checking if the enter data is valid
+  for (let el of arr) {
+    let index = el[0];
+    let data = el[1];
+    let storedData = newLL.get(index);
+
+    if (data !== storedData.data) {
+      beautifyLogs(testName, false);
+      return;
+    }
+  }
+
+  beautifyLogs(testName, true);
 })();
 // Adding Items in the end
