7cbf13b9e29da5edea770bc1665114d040e96f4e
invalidtoken <codeit.sahil98@gmail.com>
2020-07-08 00:42:01 +0530

    deleating contents of index file

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const LL = require("./constructors/linked-list");
 
-let newLL = new LL();
-// newLL.print();
-newLL.add(0, 0);
-// newLL.print();
-newLL.add(1, 1);
-// newLL.print();
-newLL.add(2, 2);
-
-newLL.add(3, 3);
-newLL.print();
-
-console.log(newLL);
